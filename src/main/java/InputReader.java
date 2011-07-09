16ff992da87c7a61abcfbf363f50a83f7f42ae70
Pooja Akshantal <pooja.akshantal@yahoo.com>
2011-07-10 00:37:28 +0530

    Tested InputReader for reading the Input

--- src/main/java/InputReader.java
+++ src/main/java/InputReader.java
@@ -1,12 +1,38 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.List;
 
 public class InputReader {
     List<Item> itemList;
 
-    public boolean inputFileExist(String file) throws FileNotFoundException {
-        File checkFile=new File(file);
-        return checkFile.exists()?true:false;
+    public boolean inputFileExist(String file){
+        try{
+        File fileName=new File(file);
+        return fileName.exists()?true:false;
+        }catch (Exception e){
+            System.out.print(e);
+        }
+        return false;
+    }
+
+    public Boolean readInputFromFile(String filename) {
+
+        if (inputFileExist(filename)){
+            BufferedReader reader= null;
+            try {
+                reader = new BufferedReader(new FileReader(filename));
+                String line=null;
+                while ((line=reader.readLine())!=null){
+                    System.out.println(line);
+
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 }
--- src/test/unit/java/InputReaderTest.java
+++ src/test/unit/java/InputReaderTest.java
@@ -15,4 +15,10 @@ public class InputReaderTest {
         InputReader inputReader=new InputReader();
         assertThat(inputReader.inputFileExist("input.txt"),is(true));
     }
+
+    @Test
+    public void shouldReadInput() throws FileNotFoundException {
+        InputReader inputReader=new InputReader();
+        assertThat(inputReader.readInputFromFile("input.txt"),is(true));
+    }
 }
