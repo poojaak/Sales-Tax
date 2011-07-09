fe811870913156e4cf12faca3bbb792983e9e392
Pooja Akshantal <pooja.akshantal@yahoo.com>
2011-07-09 23:36:43 +0530

    Modified the cucumber test

--- src/test/java/SalesTaxSteps.java
+++ src/test/java/SalesTaxSteps.java
@@ -3,26 +3,31 @@ package test.java;
 import cuke4duke.annotation.I18n.EN.Given;
 import cuke4duke.annotation.I18n.EN.Then;
 
+import java.lang.String;
+
 import static junit.framework.Assert.assertEquals;
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class SalesTaxSteps {
-    private String typeOfGoods="";
     private int salesTax=0;
 
-    @Given("^the goods are ([^\"]+)$")
-    public void assignTheGoods(String typeOfGoods) {
-        this.typeOfGoods+=" "+typeOfGoods;
-         if(typeOfGoods.contains("not"))
-            salesTax=10;
-         else if(typeOfGoods.contains("books, food and medical products"))
-            salesTax+=0;
-         if(typeOfGoods.contains("imported"))
+    @Given("^the goods are among books, food and medical products$")
+    public void assignTheGoods() {
+        salesTax+=0;
+    }
+
+    @Given("^the goods are imported$")
+    public void checkForImportedGoods() {
             salesTax+=5;
     }
 
+    @Given("^the goods are not among books, food and medical products$")
+    public void assignTheNotExemptedGoods(){
+          salesTax+=10;
+    }
+
     @Then("^the sales tax is ([^\"]+) on each good$")
-    public void theGreetingIs(String tax) {
+    public void checkSalesTax(String tax) {
         String calculatedTax="";
         if(salesTax==0)
             calculatedTax="exempted";
